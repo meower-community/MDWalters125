30da41d389a238bac51438b85467f2462cf06eab
M.D. Walters <808426@pdsb.net>
2022-09-05 13:57:39 -0400

    Update MDWalters125.js

--- MDWalters125.js
+++ MDWalters125.js
@@ -1,7 +1,7 @@
 const uptime = new Date();
 const help = ["~hello", "~help", "~str", "~amazing", "~uptime", "~uwu", "~8ball"];
 const eightBall = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
-const motd = ["Furries = Cool"];
+const motd = ["Furries = Cool", "GUYS MEOWER IS NOT DEAD"];
 
 post("MDWalters125 is now online! Use ~help to see a list of commands.");
 
