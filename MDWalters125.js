6cff2ca7a019bd3a0d125c84cdba59aa0de759fb
mdwalters <mail@mdwalters.ml>
2022-09-10 23:52:06 -0400

    remove motd, if that's the problem

--- MDWalters125.js
+++ MDWalters125.js
@@ -1,7 +1,6 @@
 const uptime = new Date();
 const help = ["~hello", "~help", "~str", "~amazing", "~uptime", "~uwu", "~8ball"];
 const eightBall = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
-const motd = ["Furries = Cool", "GUYS MEOWER IS NOT DEAD"];
 
 post("MDWalters125 is now online! Use ~help to see a list of commands.");
 
@@ -37,10 +36,5 @@ window.handlePost = function(bundle) {
     if (bundle[1].startsWith("~8ball")) {
     	var num = Math.floor(Math.random() * eightBall.length);
     	post(eightBall[num]);
-    }
-
-    if (bundle[1].startsWith("~motd")) {
-    	var num = Math.floor(Math.random() * motd.length);
-    	post(motd[num]);
     }
 }
