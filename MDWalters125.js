803fb46c7f64bfbcfe512f2071a31b0e13fa3456
M.D. Walters <808426@pdsb.net>
2022-09-19 23:31:36 -0400

    Update MDWalters125.js

--- MDWalters125.js
+++ MDWalters125.js
@@ -1,6 +1,7 @@
 const uptime = new Date();
-const help = ["~hello", "~help", "~str", "~amazing", "~uptime", "~uwu", "~8ball"];
+const help = ["~hello", "~help", "~str", "~amazing", "~uptime", "~uwu", "~8ball", "~motd"];
 const eightBall = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+const motd = ["Meower is not dead", "Furries can do infinite crime"];
 
 post("MDWalters125 is now online! Use ~help to see a list of commands.");
 
@@ -36,5 +37,10 @@ window.handlePost = function(bundle) {
     if (bundle[1].startsWith("~8ball")) {
     	var num = Math.floor(Math.random() * eightBall.length);
     	post(eightBall[num]);
+    }
+
+    if (bundle[1].startsWith("~motd")) {
+    	var num = Math.floor(Math.random() * motd.length);
+    	post(motd[num]);
     }
 }
