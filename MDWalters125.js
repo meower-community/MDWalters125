b57f0bfb964fd3bd9cf5992c1f6c2d241c9ffb14
M.D. Walters <808426@pdsb.net>
2022-10-03 13:51:35 -0400

    iFixedAFewBugs

--- MDWalters125.js
+++ MDWalters125.js
@@ -1,5 +1,5 @@
 const uptime = new Date();
-const help = ["~hello", "~help", "~str", "~amazing", "~uptime", "~uwu", "~8ball", "~motd"];
+const help = ["~hello", "~help", "~str", "~amazing", "~uptime", "~uwu", "~8ball", "~motd", "~about"];
 const eightBall = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 const motd = ["Meower is not dead", "Furries can do infinite crime", "~8ball get a life?", "Never gonna give you up", "usebottles", "Why did the chicken cross the road? To get to the other side", "Made in Canada", "The question that I always ask Bill Gates is why Windows is closed-source", "M.D. created Markdown, you can't deny that", "Prodly Furry"];
 const muted = ["Eris"];
@@ -20,7 +20,7 @@ window.handlePost = function(bundle) {
         return;
     }
 
-    if (bundle[1].startsWith("~") && !(help.includes(bundle[1]))) {
+    if (bundle[1].startsWith("~") && !(help.includes(bundle[1].split(" ")[0]))) {
         post("That command doesn't exist! Use ~help to see a list of commands.");
         return;
     }
@@ -61,5 +61,9 @@ window.handlePost = function(bundle) {
     if (bundle[1].startsWith("~motd")) {
     	var num = Math.floor(Math.random() * motd.length);
     	post(motd[num]);
+    }
+
+    if (bundle[1].startsWith("~about")) {
+    	post("MDWalters125 by @MDWalters124\n");
     }
 }
