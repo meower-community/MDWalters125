c2839a890c9e9d704c48440873513f6d2eef9b29
M.D. Walters <808426@pdsb.net>
2022-10-03 14:16:40 -0400

    Psycology Tricks on Meower

--- MDWalters125.js
+++ MDWalters125.js
@@ -1,7 +1,7 @@
 const uptime = new Date();
 const help = ["~hello", "~help", "~str", "~amazing", "~uptime", "~uwu", "~8ball", "~motd", "~restart"];
 const eightBall = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
-const motd = ["Meower is not dead", "Furries can do infinite crime", "~8ball get a life?", "Never gonna give you up", "usebottles", "Why did the chicken cross the road? To get to the other side", "Made in Canada", "The question that I always ask Bill Gates is why Windows is closed-source", "M.D. created Markdown, you can't deny that", "Proudly Furry"];
+const motd = ["Meower is not dead", "Furries can do infinite crime", "~8ball get a life?", "Never gonna give you up", "usebottles", "Why did the chicken cross the road? To get to the other side", "Made in Canada", "The question that I always ask Bill Gates is why Windows is closed-source", "M.D. created Markdown, you can't deny that", "Proudly Furry", "You are currently muted from MDWalters125.", "MDWalters125 is now online! Use ~help to see a list of commands."];
 const muted = ["Eris"];
 const admins = ["MDWalters124", "m"];
 
