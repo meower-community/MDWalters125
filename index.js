26ae70e6db9f238b0ec9b0cbe0780d0621752f6e
mdwalters <mail@mdwalters.ml>
2022-11-01 19:29:37 -0400

    Merge `~emoji` with `~say`

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const username = process.env.MDW125_USERNAME;
 const password = process.env.MDW125_PASSWORD;
 
 const uptime = new Date().getTime();
-const help = ["~hello", "~help", "~say", "~amazing", "~uptime", "~uwu", "~8ball", "~motd", "~zen", "~shorten", "~cat", "~status", "~random", "~clap", "~emojify"];
+const help = ["~hello", "~help", "~say", "~amazing", "~uptime", "~uwu", "~8ball", "~motd", "~zen", "~shorten", "~cat", "~status", "~random", "~clap"];
 const eightBall = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 const motd = ["Meower is not dead", "Furries can do infinite crime", "~8ball get a life?", "Never gonna give you up", "usebottles", "Why did the chicken cross the road? To get to the other side", "Made in Canada", "The question that I always ask Bill Gates is why Windows is closed-source", "M.D. created Markdown, you can't deny that", "Proudly Furry", "You are currently muted from MDWalters125.", "MDWalters125 is now online! Use ~help to see a list of commands.", "Hello from Node.js!"];
 const muted = ["Eris"];
@@ -104,7 +104,7 @@ async function handlePost(user, message) {
     }
 
     if (message.startsWith("~say")) {
-        post(message.split(" ").slice(1, message.split(" ").length).join(" "));
+        post(emoji.emojify(message.split(" ").slice(1, message.split(" ").length).join(" ")));
     }
 
     if (message.startsWith("~amazing")) {
@@ -187,10 +187,6 @@ async function handlePost(user, message) {
 
     if (message.startsWith("~clap")) {
     	post("👏");
-    }
-
-    if (message.startsWith("~emojify")) {
-    	post(emoji.emojify(message.split(" ").slice(1, message.split(" ").length).join(" ")));
     }
 }
 
