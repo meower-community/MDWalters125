8578fb2f73fb0579f5b4bbc2ac21c4850704fbd0
mdwalters <mail@mdwalters.ml>
2022-11-01 18:07:56 -0400

    `~clap`

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const username = process.env.MDW125_USERNAME;
 const password = process.env.MDW125_PASSWORD;
 
 const uptime = new Date().getTime();
-const help = ["~hello", "~help", "~say", "~amazing", "~uptime", "~uwu", "~8ball", "~motd", "~zen", "~shorten", "~cat", "~status", "~random"];
+const help = ["~hello", "~help", "~say", "~amazing", "~uptime", "~uwu", "~8ball", "~motd", "~zen", "~shorten", "~cat", "~status", "~random", "~clap"];
 const eightBall = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 const motd = ["Meower is not dead", "Furries can do infinite crime", "~8ball get a life?", "Never gonna give you up", "usebottles", "Why did the chicken cross the road? To get to the other side", "Made in Canada", "The question that I always ask Bill Gates is why Windows is closed-source", "M.D. created Markdown, you can't deny that", "Proudly Furry", "You are currently muted from MDWalters125.", "MDWalters125 is now online! Use ~help to see a list of commands.", "Hello from Node.js!"];
 const muted = ["Eris"];
@@ -183,6 +183,10 @@ async function handlePost(user, message) {
             post("~random word, ~random dice, ~random number");
         }
     }
+
+    if (message.startsWith("~clap")) {
+    	post("👏");
+    }
 }
 
 function post(content) {
@@ -214,8 +218,12 @@ ws.on('message', function message(data) {
     var messageData = `${data}`;
     var messageData = JSON.parse(messageData);
     if (messageData.val.type === 1) {
-        console.log(`${messageData.val.u}: ${messageData.val.p}`);
-        handlePost(messageData.val.u, messageData.val.p);
+        try {
+            console.log(`${messageData.val.u}: ${messageData.val.p}`);
+            handlePost(messageData.val.u, messageData.val.p);
+        } catch(error) {
+            post
+        }
     } else if (messageData.cmd === "ping") {
         if (messageData.val === "I:100 | OK") {
             console.log("Ping is OK");
