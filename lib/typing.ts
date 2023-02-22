acb08d4c649c9a14c056f9df5ed483701d90e9ea
M.D. Walters <popularvsofficial@gmail.com>
2023-02-22 18:12:10 -0500

    yes

--- lib/typing.ts
+++ lib/typing.ts
@@ -1,3 +1,3 @@
-export function typing(bot): void {
+export function typing(bot: Bot): void {
     bot.send({ "cmd": "direct", "val": { "cmd": "set_chat_state", "val": { "chatid": "livechat", "state": 101, } } });
 }
