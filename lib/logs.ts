743a5ece9e9c657289e650fec949a7908d53a736
M.D. Walters <popularvsofficial@gmail.com>
2023-03-20 20:22:54 -0400

    OK, jeez

--- lib/logs.ts
+++ lib/logs.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
 
 export function log(content: string): void {
-    fs.writeFile("logs", `${content}\n`, { flag: "a+" }, (err) => { return; });
+    fs.writeFile("logs", `${content}\n`, { flag: "a+" }, () => { return; });
 }
