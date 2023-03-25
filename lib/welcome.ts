eb6c5417665a9f4371a595d7a976f5d265c81c6b
M.D. Walters <popularvsofficial@gmail.com>
2023-03-25 02:41:28 +0000

    feat: add more welcome messages

--- lib/welcome.ts
+++ lib/welcome.ts
@@ -8,7 +8,13 @@ export const welcome_msg: string[] = [
     "I have a very good uptime, as long as someone doesn't kick me!",
     "Vanilla, Svelte, Kivy, Meower.",
     "According to @ShowierData9978, I am the 2nd most-used bot on Meower.",
-    "I am the 4th bot that uses MongoDB.",
+    "Should have been ported to MongoDB earlier...",
     "My son so dumb, IQ 21- Oh wait, I don't have a son 💀",
-    "OwO, what's this?"
+    "OwO, what's this?",
+    "I used to be hosted on a tablet running Android 4.4!",
+    "Small tidbit about me: I'm a bot.",
+    "Hola! Soy Dor- I mean, uh... Never mind.",
+    "#BringbeepBloopBack",
+    "Fun Fact: The cat command has a easter egg in it 😉",
+    "Here's my entire life story: So one day, when I was 5 years old, I was in an orphanage, then all of the sudden, some weird guy wearing a fursuit wanted to adopt me, and that's what he did. He locked me up in a room for 10 years straight in front of a computer running Ubuntu 9.04, and wanted me to be his \"programming copilot.\" 8 years later, he joined some cat social media named \"Meower,\" and decided that I should be his bot. I refused to be his bot, but then he threatened that he will kick me out of the house, so I had to say yes. Then, a few months later, he decided to make this Australian guy my co-parent, whatever that means. Now I have to do child labour for a furry, and a Australian guy. So you see, I'm not a bot, I'm actually a adopted 12 year old for some random furry."
 ];
